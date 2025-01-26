174fb8bd11fc2cbd440243176f97fc85cf285844
Paul Christianson <pacdeveloping@gmail.com>
2025-01-26 11:51:46 -0500

    added folder for pdfs, return json of filenames to client

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
+
+const pdfdir = path.join(__dirname, 'pdfs')
+
 require('dotenv').config();
 
 const app = express();
@@ -8,10 +13,20 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    fs.readdir(pdfdir, (err, files) => {
+        if (err) {
+            res.status(500).json({error: err.message});
+            return;
+        }
+        res.json({dir: pdfdir, pdfs: files});
+    });
+});
+
 app.get('/api/test', (req, res) => {
-  res.json({ message: 'Hello from Express!' });
+    res.json({ message: 'Hello from Express!' });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
