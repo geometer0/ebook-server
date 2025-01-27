6d23f6d127b034f282751d1f9368e1795eb5f98c
Paul Christianson <me@paulchristianson.io>
2025-01-27 00:03:50 -0500

    Removed remaining template code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,9 @@
 import { useState,useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 
 
 function App() {
-  const [count, setCount] = useState(0)
   const [pdfs, setPdfs] = useState([])
 
   useEffect(() => {
@@ -18,26 +15,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Library</h1>
       <ul>
         {pdfs.map(pdf => (
           <li key={pdf}><a target="_blank" href={'pdfs/' + pdf}>{pdf.slice(0,-4)}</a></li>
