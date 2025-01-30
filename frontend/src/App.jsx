090cc096a263bf066417c788deb24c970686438e
Paul Christianson <me@paulchristianson.io>
2025-01-29 20:54:02 -0500

    Hardcoded local IP to allow testing on mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react'
 import PDFViewer from './PDFViewer'
 import './App.css'
+const backend = 'http://192.168.1.183:3000/'
 
 function App() {
   const [pdfs, setPdfs] = useState([]);
   const [selectedPDF, setSelectedPDF] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:3000')
+    fetch(backend)
       .then(res => res.json())
       .then(data => setPdfs(data.pdfs))
       .catch(err => console.error('Error fetching PDFs:', err));
   }, []);
 
   if (selectedPDF) {
-    const encodedPdfUrl = 'http://localhost:3000/pdfs/' + encodeURIComponent(selectedPDF);
+    const encodedPdfUrl = backend + 'pdfs/' + encodeURIComponent(selectedPDF);
     return (
       <div>
         <button onClick={() => setSelectedPDF(null)}>← Back to Library</button>
